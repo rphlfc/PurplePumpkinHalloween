0323b87ba02fbb0be742a2783155320ef385a1b3
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-11-07 20:22:03 -0300

    Initial Commit

---
+++ PurplePumpkinHalloween/PurplePumpkinHalloweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurplePumpkinHalloweenApp.swift
+//  PurplePumpkinHalloween
+//
+//  Created by Raphael Cerqueira on 07/11/20.
+//
+
+import SwiftUI
+
+@main
+struct PurplePumpkinHalloweenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
